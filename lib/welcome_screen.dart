06fd39a9bfe83d8ab640606574eb4415e9e34cc8
sailohitaksh-cryptic <sd_be20@thapar.edu>
2021-07-31 11:38:24 +0530

    Update welcome_screen.dart

--- lib/welcome_screen.dart
+++ lib/welcome_screen.dart
@@ -3,9 +3,8 @@ import 'package:google_fonts/google_fonts.dart';
 import 'login.dart';
 import 'signup1.dart';
 
-
 class WelcomeScreen extends StatefulWidget {
-  static String id ='welcome_screen';
+  static String id = 'welcome_screen';
   @override
   _WelcomeScreenState createState() => _WelcomeScreenState();
 }
@@ -21,19 +20,19 @@ class _WelcomeScreenState extends State<WelcomeScreen> {
           Expanded(
             flex: 6,
             child: Stack(
-              children:[
+              children: [
                 Container(
                   decoration: BoxDecoration(
                     image: DecorationImage(
-                      image: NetworkImage('https://i.pinimg.com/originals/a0/dd/e7/a0dde7e45b02c341c77d0d048a26c291.gif'),
+                      image: NetworkImage(
+                          'https://i.pinimg.com/originals/a0/dd/e7/a0dde7e45b02c341c77d0d048a26c291.gif'),
                       fit: BoxFit.fill,
                     ),
                   ),
                 ),
-
                 Container(
                   child: Padding(
-                    padding: const EdgeInsets.only(top:88.0,left:35.0),
+                    padding: const EdgeInsets.only(top: 88.0, left: 35.0),
                     child: Text(
                       'Eventaholic',
                       style: TextStyle(
@@ -42,7 +41,6 @@ class _WelcomeScreenState extends State<WelcomeScreen> {
                         fontSize: 70.0,
                         fontWeight: FontWeight.w400,
                       ),
-
                     ),
                   ),
                 )
@@ -51,79 +49,67 @@ class _WelcomeScreenState extends State<WelcomeScreen> {
           ),
           Expanded(
               child: Padding(
-                padding: const EdgeInsets.all(15.0),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                  children: [
-                    Expanded(
-                      child: GestureDetector(
-                        onTap: (){
-                          Navigator.pushNamed(context, LoginScreen.id);
-                        },
-                        child: Container(
-                          margin: EdgeInsets.only(right: 5),
-                          height: 50,
-                          width: 30,
-                          decoration: BoxDecoration(
-                            border: Border.all(
-                              color: Colors.black,
-                            ),
-                            borderRadius: BorderRadius.circular(30),
-
-                          ),
-                          child: Center(
-                            child: Text(
-                              'Login',
-                              style: GoogleFonts.poppins(
-                                color: Colors.black,
-                                fontSize: 20.0,
-                                fontWeight: FontWeight.w500,
-                              ),
-                            ),
-                          ),
-
-
+            padding: const EdgeInsets.all(15.0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                Expanded(
+                    child: GestureDetector(
+                  onTap: () {
+                    Navigator.pushNamed(context, LoginScreen.id);
+                  },
+                  child: Container(
+                    margin: EdgeInsets.only(right: 5),
+                    height: 50,
+                    width: 30,
+                    decoration: BoxDecoration(
+                      border: Border.all(
+                        color: Colors.black,
+                      ),
+                      borderRadius: BorderRadius.circular(30),
+                    ),
+                    child: Center(
+                      child: Text(
+                        'Login',
+                        style: GoogleFonts.poppins(
+                          color: Colors.black,
+                          fontSize: 20.0,
+                          fontWeight: FontWeight.w500,
                         ),
-                      )
+                      ),
                     ),
-                    Expanded(
-                        child: GestureDetector(
-                          onTap: (){
-                            Navigator.pushNamed(context, SignUpScreen.id);
-                          },
-                          child: Container(
-
-                            margin: EdgeInsets.only(left: 5),
-                            height: 50,
-                            width: 30,
-                            decoration: BoxDecoration(
-                              color: Colors.black,
-                              borderRadius: BorderRadius.circular(30),
-                            ),
-                            child: Center(
-                              child: Text(
-                                'Sign Up',
-                                style: GoogleFonts.poppins(
-                                  color: Colors.white,
-                                  fontSize: 20.0,
-                                  fontWeight: FontWeight.w500,
-                                ),
-                              ),
-                            ),
-
-                          ),
-                        )
+                  ),
+                )),
+                Expanded(
+                    child: GestureDetector(
+                  onTap: () {
+                    Navigator.pushNamed(context, SignUpScreen.id);
+                  },
+                  child: Container(
+                    margin: EdgeInsets.only(left: 5),
+                    height: 50,
+                    width: 30,
+                    decoration: BoxDecoration(
+                      color: Colors.black,
+                      borderRadius: BorderRadius.circular(30),
                     ),
-
-                  ],
-                ),
-              )
-          )
+                    child: Center(
+                      child: Text(
+                        'Sign Up',
+                        style: GoogleFonts.poppins(
+                          color: Colors.white,
+                          fontSize: 20.0,
+                          fontWeight: FontWeight.w500,
+                        ),
+                      ),
+                    ),
+                  ),
+                )),
+              ],
+            ),
+          ))
         ],
       ),
-
-
     );
   }
 }
-
